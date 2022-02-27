bc1cef7d77fffe921fe8909f1e9cd974e9448530
neayom <neayom@qq.com>
2022-02-27 12:01:35 +0800

    20220227v1

--- src/com/dlnu/Person.java
+++ src/com/dlnu/Person.java
@@ -1,9 +1,7 @@
 package com.dlnu;
 
 public class Person {
-    private String id;
     private String name;
     private Integer age;
-    private String address;
-    private String abc;
+    private String id;
 }
